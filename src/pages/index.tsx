25069ee599035c82ce7a10587d2ddb24842615c4
Oleksii Lukovka <lukerlexa@gmail.com>
2023-01-23 21:36:05 +0200

    [MSYA-46]: refactor head (#6)
    * [MSYA-46]: create AppHead.tsx and add for the global import
    
    * [MSYA-46]: use AppHead on the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import { type NextPage } from 'next';
-import Head from 'next/head';
 import { api } from 'src/utils/api';
+import { AppHead } from '../components';
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: 'from tRPC' });
 
   return (
     <>
-      <Head>
-        <title>Missiya</title>
-        <link rel='icon' href='/logo.svg' />
-      </Head>
+      <AppHead />
       <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]'>
         <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16'>
           <div className='flex flex-col items-center gap-2 divide-y'>
