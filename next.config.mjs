9ca944dccdd61b5d53ae526e6d6112ea402b2af6
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2023-01-27 19:59:44 +0400

    [MSYA-46]: skip types validation

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@
 const config = {
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
