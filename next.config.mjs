7cb5a2e9133a0470e32da3fa69fe5c7ddfff0935
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2023-01-27 21:21:05 +0400

    [MSYA-46]: fix build

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const config = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
+    // ignoreBuildErrors: true,
   },
   i18n: {
     locales: ['en'],
