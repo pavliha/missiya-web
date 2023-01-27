d8fd8d728ed278c94d260574473158dd743a28a9
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2023-01-27 21:29:50 +0400

    [MSYA-46] fix deploy (#9)
    * [MSYA-46]: skip types validation
    
    * [MSYA-46]: fix build

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class', // or 'media' or 'class'
 };
