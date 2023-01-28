09b6faa4b3e55056588b15cb4f3c7d179f24268b
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2023-01-28 21:53:54 +0400

    [MSYA-52]: test free ports on netlify

--- src/pages/api/connect.ts
+++ src/pages/api/connect.ts
@@ -1,7 +1,10 @@
+// noinspection ExceptionCaughtLocallyJS
+
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { setCorsHeaders } from 'src/utils/setCorsHeaders';
 import { METHODS } from 'src/server/constants';
 import { prisma } from 'src/server/db';
+import { findFreePorts } from 'find-free-ports';
 
 interface ConnectRequestBody {
   droneID: string;
@@ -19,7 +22,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         droneID,
       },
     });
-    res.status(200).json({ port: 8088 });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+    const ports = await findFreePorts(4);
+
+    res.status(200).json({
+      port: JSON.stringify(ports),
+    });
   } catch (error: unknown) {
     res.status(400).json({ error: (error as Error).message });
   } finally {
