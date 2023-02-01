352caf740a5fadfd4e332832a7ab3c65f3f611d3
Oleksii Lukovka <lukerlexa@gmail.com>
2023-02-01 08:24:01 +0200

    [MSYA-51]: add InputMask with mask for the drone id (#12)

--- src/components/DroneIDForm.tsx
+++ src/components/DroneIDForm.tsx
@@ -1,5 +1,6 @@
 import { type FC } from 'react';
 import { useForm } from 'react-hook-form';
+import InputMask from 'react-input-mask';
 
 export interface VideoFormData {
   droneID: string;
@@ -19,17 +20,18 @@ export const DroneIDForm: FC<Props> = ({ onSubmit }) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
       <p className="mt-2 text-white">Enter your DRONE ID to see video from drone</p>
-      <input
+      <InputMask
+        mask="AA99A9999999"
         className="mt-2 w-full rounded-md bg-white px-3 py-2"
-        placeholder="Drone ID"
-        {...register('droneID', { required: true, pattern: /^[A-Z]{3}-[0-9]{6}/ })}
+        placeholder="Example Drone ID: MQ23Z0000001"
+        {...register('droneID', { required: true, pattern: /^[A-Z]{2}[0-9]{2}[A-Z][0-9]{7}/ })}
       />
       {errors.droneID?.type === 'required' && <span className="text-red-800">This field is required</span>}
       {errors.droneID?.type === 'pattern' && <span className="text-red-800">Wrong format</span>}
 
       <button
         type="submit"
-        className="mt-2 rounded-md bg-violet-500 px-3 py-2 uppercase hover:bg-violet-600 active:bg-violet-700"
+        className="mt-3 rounded-md bg-violet-500 px-3 py-2 uppercase hover:bg-violet-600 active:bg-violet-700"
       >
         Open video stream
       </button>
